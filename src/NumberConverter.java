308bd7728373253710eda4e43acb0eccef4362e0
Sifat <sanan6576@bths.edu>
2023-01-28 23:50:53 -0500

    almost done

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -68,6 +68,32 @@ public class NumberConverter {
         return octal;
     }
 
+    public static String convertDecimalToBaseX(int decimal, int base){
+        String baseXValue = "";
+        if (base == 1){
+            for (int i = 0; i < decimal; i++){
+                baseXValue += "1";
+            }
+            return baseXValue;
+        }
+        else if (decimal == 0) return "0";
+        String values = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+        // finding the highest power of base X that can go into decimal:
+        int power = 0;
+        while (decimal - Math.pow(base, power) >= 0) power++;
+        if (decimal - Math.pow(base, power) < 0) power--;
+        // adding the characters to baseXValue:
+        int quotient;
+        while (power >= 0){
+            quotient = decimal / (int) Math.pow(base, power);
+            baseXValue += values.substring(quotient, quotient + 1);
+            decimal -= Math.pow(base, power) * quotient;
+            power--;
+        }
+        return baseXValue;
+    }
+
+
     public static boolean checkInputs(int base, int num){
         String number = num + "";
         if (base == 2){
@@ -78,9 +104,10 @@ public class NumberConverter {
         }
         if (base == 8){
             for (int i = 0; i < number.length(); i++){
-                if (number.charAt(i) > 8) return false;
+                if (number.charAt(i) > '7') return false; // changed from '8' to '7'
             }
         }
         return true;
     }
+
 }
